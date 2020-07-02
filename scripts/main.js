952261cf8bb8043de4b4690ac55d3a050367f505
Coriyon <coriyonarrington@gmail.com>
2020-07-02 05:45:54 -0500

    updated scroll feature

--- scripts/main.js
+++ scripts/main.js
@@ -28,28 +28,49 @@ function closeNav() {
 // this opens and closes the project nav menu
 function openMenu() {
     document.getElementById("projectNav").style.height = "100vh";
+    document.getElementById("html").style.overflow = "hidden";
+    document.getElementById("project-page").style.overflow = "visible";
+    document.getElementById("project-page").style.height = "100%";
 }
 
 function closeMenu() {
+    document.getElementById("html").style.overflow = "visible";
     document.getElementById("projectNav").style.height = "0%";
 }
 
-// this controls the logo appear/disappear
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("mobile-avatar").style.display = "block";
-    document.getElementById("mobile-avatar").style.transition = "0.3s ease-in-out";
-    document.getElementById("mobile-avatar").style.bottom = ".25vh";
-    // document.getElementById("project-nav-icon").style.display = "block";
-    // document.getElementById("project-nav-icon").style.transition = "0.3s ease-in-out";
-  } else {
-    document.getElementById("mobile-avatar").style.display = "none";
-    // document.getElementById("project-nav-icon").style.display = "none";
+function debounce(func, wait = 10, immediate = true) {
+    let timeout;
+    return function() {
+      let context = this, args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  let scrollPos = 0;
+  const nav = document.querySelector('#mobile-avatar');
+
+  function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY < scrollPos) {
+      // Scrolling UP
+      nav.classList.add('is-visible');
+      nav.classList.remove('is-hidden');
+    } else {
+      // Scrolling DOWN
+      nav.classList.add('is-hidden');
+      nav.classList.remove('is-visible');
+    }
+    scrollPos = windowY;
   }
-  prevScrollpos = currentScrollPos;
-}
+
+window.addEventListener('scroll', debounce(checkPosition));
 
 // this changes the cursor for desktop
 
